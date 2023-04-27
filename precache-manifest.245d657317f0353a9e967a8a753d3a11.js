fdcfc004c3e249db70417fd6fdde811a5915a51f
rafael.lumang <lumang.rafael@gmail.com>
2023-04-27 15:12:47 -0700

    Updates

---
+++ precache-manifest.245d657317f0353a9e967a8a753d3a11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b7776284e5fc2a727f5",
+    "url": "/course-directory/static/css/main.47e0f0f5.chunk.css"
+  },
+  {
+    "revision": "2b7776284e5fc2a727f5",
+    "url": "/course-directory/static/js/main.2b777628.chunk.js"
+  },
+  {
+    "revision": "25a97381f2fd20d18f35",
+    "url": "/course-directory/static/js/1.25a97381.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "6819fef8426537697c5ced53477b32b2",
+    "url": "/course-directory/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/course-directory/precache-manifest.92c7fcdc01936e03a4dee63ee66f8c95.js"
+  "/course-directory/precache-manifest.245d657317f0353a9e967a8a753d3a11.js"
 );
 
 workbox.clientsClaim();
